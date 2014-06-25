9cea1399556c6e9eb6d707cfed7059e2f5bea8cb
Richard Spindler <richard@lateralblast.com.au>
2014-06-26 04:29:26 +1000

    Added MPlayerX

---
+++ apps/mplayerx.rb
@@ -0,0 +1,38 @@
+#
+# Routines for MPlayerX
+#
+
+def get_mplayerx_app_name()
+  app_name = "MPlayerX"
+  return app_name
+end
+
+def get_mplayerx_app_url()
+  app_url = "http://mplayerx.org/download.html"
+  return app_url
+end
+
+def get_mplayerx_pkg_url(app_url)
+  pkg_url = Net::HTTP.get(URI.parse(app_url)).split("\n").grep(/zip/)[0].split(/"/)[1]
+  return pkg_url
+end
+
+def get_mplayerx_rem_ver(app_url)
+  pkg_url = get_mplayerx_pkg_url(app_url)
+  rem_ver = File.basename(pkg_url,".zip").split(/-/)[1]
+  return rem_ver
+end
+
+def get_mplayerx_pkg_type()
+  pkg_type = "zip"
+  return pkg_type
+end
+
+def get_mplayerx_loc_ver(app_name)
+  loc_ver = get_app_ver(app_name)
+  return loc_ver
+end
+
+def do_mplayerx_post_install(app_name)
+  return
+end
