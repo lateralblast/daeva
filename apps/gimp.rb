5640136335513a6422e64939ef84a1819e5cb94b
Richard Spindler <richard@lateralblast.com.au>
2014-09-15 13:11:48 +1000

    Revert "Mon 15 Sep 2014 13:07:42 AEST"
    This reverts commit a9723a6f53202f43f3221407efe3f644e632a4d9.

--- apps/gimp.rb
+++ apps/gimp.rb
@@ -13,28 +13,31 @@ def get_gimp_app_type()
 end
 
 def get_gimp_app_url()
-  app_url = "http://www.macupdate.com/app/mac/14597/gimp"
+  app_url = "http://www.partha.com/"
   return app_url
 end
 
 def get_gimp_pkg_url(app_name,app_url)
-  pkg_url = get_macupdate_url(app_name,app_url)
+  pkg_url = Net::HTTP.get(URI.parse(app_url)).split(/\n/).grep(/zip/)[1].split(/"/)[1]
+  pkg_url = app_url+pkg_url
   return pkg_url
 end
 
 def get_gimp_rem_ver(app_name,app_url)
-  rem_ver = get_macupdate_ver(app_name,app_url)
-  return rem_ver
+  pkg_url  = get_gimp_pkg_url(app_name,app_url)
+  rem_date = File.basename(pkg_url,".zip")
+  rem_date = rem_date.split(/-/)[2]
+  rem_date = DateTime.strptime(rem_date,'%b%d%Y').to_date
+  return rem_date
 end
 
 def get_gimp_pkg_type()
-  pkg_type = "dmp"
+  pkg_type = "zip"
   return pkg_type
 end
 
 def get_gimp_loc_ver(app_name)
-  loc_ver = get_app_ver(app_name)
-  loc_ver = loc_ver.split(/,/)[0]
+  loc_ver = get_app_date(app_name)
   return loc_ver
 end
 
