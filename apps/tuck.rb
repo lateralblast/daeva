88084b9fade6fd5d852a475a4d05c78c7a142cb3
Richard Spindler <richard@lateralblast.com.au>
2015-05-02 09:11:54 +1000

    Added support for Tuck

---
+++ apps/tuck.rb
@@ -0,0 +1,42 @@
+#
+# Routines for tuck
+#
+
+def get_tuck_app_name()
+  app_name = "Tuck"
+  return app_name
+end
+
+def get_tuck_app_type()
+  app_type = "app"
+  return app_type
+end
+
+def get_tuck_app_url()
+  app_url = "http://www.macupdate.com/app/mac/47139/tuck"
+  return app_url
+end
+
+def get_tuck_pkg_url(app_name,app_url)
+  pkg_url = get_macupdate_url(app_name,app_url)
+  return pkg_url
+end
+
+def get_tuck_rem_ver(app_name,app_url)
+  rem_ver = get_macupdate_ver(app_name,app_url)
+  return rem_ver
+end
+
+def get_tuck_pkg_type()
+  pkg_type = "zip"
+  return pkg_type
+end
+
+def get_tuck_loc_ver(app_name)
+  loc_ver = get_app_ver(app_name)
+  return loc_ver
+end
+
+def do_tuck_post_install(app_name,app_url)
+  return
+end
