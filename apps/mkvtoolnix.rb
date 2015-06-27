4e7e9eff31aff1b4fc14bfbcb0a31da0a54559bc
Richard Spindler <richard@lateralblast.com.au>
2015-06-27 13:12:05 +1000

    Added support for MKVtoolnix

---
+++ apps/mkvtoolnix.rb
@@ -0,0 +1,42 @@
+#
+# Routines for MKVtoolnix
+#
+
+def get_mkvtoolnix_app_name()
+  app_name = "MKVtoolnix"
+  return app_name
+end
+
+def get_mkvtoolnix_app_type()
+  app_type = "app"
+  return app_type
+end
+
+def get_mkvtoolnix_app_url()
+  app_url = "http://www.macupdate.com/app/mac/16837/mkvtoolnix"
+  return app_url
+end
+
+def get_mkvtoolnix_pkg_url(app_name,app_url)
+  pkg_url = get_macupdate_url(app_name,app_url)
+  return pkg_url
+end
+
+def get_mkvtoolnix_rem_ver(app_name,app_url)
+  rem_ver = get_macupdate_ver(app_name,app_url)
+  return rem_ver
+end
+
+def get_mkvtoolnix_pkg_type()
+  pkg_type = "dmg"
+  return pkg_type
+end
+
+def get_mkvtoolnix_loc_ver(app_name)
+  loc_ver = get_app_ver(app_name)
+  return loc_ver
+end
+
+def do_mkvtoolnix_post_install(app_name,app_url)
+  return
+end
