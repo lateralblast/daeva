931d5b6403cded32621e8dee31a3b5f7e095295e
Richard Spindler <richard@lateralblast.com.au>
2015-07-11 13:13:59 +1000

    Updated Firefox support

--- apps/firefox.rb
+++ apps/firefox.rb
@@ -13,19 +13,17 @@ def get_firefox_app_type()
 end
 
 def get_firefox_app_url()
-  app_url = "https://www.mozilla.org/en-US/firefox/new/"
+  app_url = "http://www.macupdate.com/app/mac/10700/firefox"
   return app_url
 end
 
 def get_firefox_pkg_url(app_name,app_url)
-  pkg_url = Net::HTTP.get(URI.parse(app_url)).split("\n").grep(/osx/)[0].split(/href="/)[4].split(/"/)[0].gsub(/amp;/,"")
+  pkg_url = get_macupdate_url(app_name,app_url)
   return pkg_url
 end
 
 def get_firefox_rem_ver(app_name,app_url)
-  pkg_url = get_firefox_pkg_url(app_name,app_url)
-  rem_ver = File.basename(pkg_url)
-  rem_ver = rem_ver.split(/-/)[1]
+  rem_ver = get_macupdate_ver(app_name,app_url)
   return rem_ver
 end
 
